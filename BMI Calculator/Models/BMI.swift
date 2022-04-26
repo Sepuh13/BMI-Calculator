c0ded6a44cbdd9224da53069ac0261638fc51279
Sepuh <sepuh13@icloud.com>
2022-04-26 16:54:15 +0400

    BMI Calculator
    BMI Calculator

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  BMI Calculator
+//
+//  Created by Sepuh Baghdasaryan on 12.02.22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
